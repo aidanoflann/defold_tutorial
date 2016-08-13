8d0278ab38042a8ac83e7d5ad37456a802fc7584
Aidan <aidanoflann@gmail.com>
2016-08-13 15:57:54 +0100

    refactor and created controller.script

---
+++ hero/hero.go
@@ -0,0 +1,85 @@
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"hero\"\n"
+  "mask: \"geometry\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_SPHERE\n"
+  "    position {\n"
+  "      x: 66.47743\n"
+  "      y: 48.66491\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 1\n"
+  "  }\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: 55.469467\n"
+  "      y: 17.990097\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 1\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 25.0\n"
+  "  data: 16.829575\n"
+  "  data: 16.662998\n"
+  "  data: 10.0\n"
+  "}\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "spinemodel"
+  type: "spinemodel"
+  data: "spine_scene: \"/hero/hero.spinescene\"\n"
+  "default_animation: \"run_right\"\n"
+  "skin: \"\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  "material: \"/builtins/materials/spine.material\"\n"
+  ""
+  position {
+    x: 52.47877
+    y: 5.214485
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
